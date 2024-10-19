2bb4206616306ec9c63bb194abe85f452625a055
kozak <kozak@MacBook-Pro-kozak.local>
2024-10-19 12:41:40 +0300

    Integrated eslint

--- src/cli.js
+++ src/cli.js
@@ -1,22 +1,22 @@
 import process from 'node:process';
 import { Command } from 'commander';
-import pageLoader from "./page-loader.js";
+import pageLoader from './page-loader.js';
 
 const cli = new Command();
 
 cli
-    .version('1.0.0')
-    .usage('[options] <url>')
-    .description('Page loader utility.')
-    .option('-o, --output [dir]', 'output dir', process.cwd());
+  .version('1.0.0')
+  .usage('[options] <url>')
+  .description('Page loader utility.')
+  .option('-o, --output [dir]', 'output dir', process.cwd());
 
 cli
-    .arguments('<url>')
-    .action((url, { output }) => {
-        pageLoader(url, output).then((fileName) => {
-            console.log(`open ${fileName}`);
-        });
+  .arguments('<url>')
+  .action((url, { output }) => {
+    pageLoader(url, output).then((fileName) => {
+      console.log(`open ${fileName}`);
     });
+  });
 
 cli.parse();
 
--- src/page-loader.js
+++ src/page-loader.js
@@ -1,36 +1,26 @@
 import axios from 'axios';
 import { promises as fs } from 'fs';
 
-const fetchFile = (url) => {
-    return axios.get(url);
-}
+const fetchFile = (url) => axios.get(url);
 
-const writeFile = (pathName, data) => {
-    return fs.writeFile(pathName, data);
-}
+const writeFile = (pathName, data) => fs.writeFile(pathName, data);
 
 const getFileName = (url) => {
-    const urlWithoutProtocol = url.replace(/^https?:\/\//, '');
-    const urlWithoutSpecialChars = urlWithoutProtocol.replace(/\W/g, '-');
-    return `${urlWithoutSpecialChars}.html`;
-}
+  const urlWithoutProtocol = url.replace(/^https?:\/\//, '');
+  const urlWithoutSpecialChars = urlWithoutProtocol.replace(/\W/g, '-');
+  return `${urlWithoutSpecialChars}.html`;
+};
 
 const pageLoader = (url, output) => {
-    const fileName = getFileName(url);
-    const pathName = `${output}/${fileName}`;
+  const fileName = getFileName(url);
+  const pathName = `${output}/${fileName}`;
 
-    return fetchFile(url).then((response) => {
-            return response.data;
-        })
-        .then((data) => {
-            return writeFile(pathName, data);
-        })
-        .then(() => {
-            return pathName;
-        })
-        .catch((error) => {
-            console.error(error);
-        });
+  return fetchFile(url).then((response) => response.data)
+    .then((data) => writeFile(pathName, data))
+    .then(() => pathName)
+    .catch((error) => {
+      console.error(error);
+    });
 };
 
 export default pageLoader;
